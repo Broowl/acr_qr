63dba6e7b7bb97b4d41801b693ae5f93108be351
Broowl <daniel.krieger95@gmail.com>
2023-07-31 02:51:39 +0200

    allow multiple files

--- generator/main.py
+++ generator/main.py
@@ -1,19 +1,25 @@
 import crypto
 import qr
 import argparse
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("event_name")
-    parser.add_argument("file_name")
+    parser.add_argument("out_dir")
+    parser.add_argument("num_codes")
     parser.add_argument("-puk", "--public_key")
     parser.add_argument("-prk", "--private_key")
     parsed_args = parser.parse_args()
     event_name = parsed_args.event_name
-    file_name = parsed_args.file_name
+    out_dir = parsed_args.out_dir
+    num_codes = int(parsed_args.num_codes)
     public_key_file = parsed_args.public_key
     private_key_file = parsed_args.private_key
-    args = parser.parse_args()
+
     key = crypto.generate_or_get_keys(private_key_file, public_key_file)
-    signature = crypto.sign_message(event_name, key)
-    qr.save_signed_message(event_name, signature, file_name)
+    for i_code in range(num_codes):
+        data = f"{event_name}_{i_code}"
+        signature = crypto.sign_message(data, key)
+        qr.save_signed_message(data, signature, os.path.join(
+            out_dir, event_name, f"{i_code}.png"))
--- scanner/main.py
+++ scanner/main.py
@@ -1,18 +1,21 @@
 import crypto
 import qr
 import argparse
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_name")
+    parser.add_argument("in_dir")
     parser.add_argument("public_key")
     parsed_args = parser.parse_args()
-    file_name = parsed_args.file_name
+    in_dir = parsed_args.in_dir
     public_key_file = parsed_args.public_key
+
     key = crypto.read_key(public_key_file)
-    read_message, read_signature = qr.read_signed_message(file_name)
-    is_verified = crypto.verify_message(read_message, read_signature, key)
-    if is_verified:
-        print("Access granted")
-    else:
-        print("Access denied")
+    for file_name in os.listdir(in_dir):
+        read_message, read_signature = qr.read_signed_message(os.path.join(in_dir, file_name))
+        is_verified = crypto.verify_message(read_message, read_signature, key)
+        if is_verified:
+            print("Access granted")
+        else:
+            print("Access denied")

b53382da8cb73217243a2f88673f15c8da99457c
Broowl <32833291+Broowl@users.noreply.github.com>
2023-08-13 21:16:34 +0200

    create one log file per event (#14)

--- src/scanner/scanner_lib/id_storage.py
+++ src/scanner/scanner_lib/id_storage.py
@@ -1,10 +1,14 @@
 import os
 from pathlib import Path
 import time
+import csv
+from datetime import datetime, timezone
 from io import TextIOWrapper
 from types import TracebackType
 from typing import Dict, Optional, Any
+import iso8601
 from .persistence import Persistence
+
 
 class IdStorage:
     """Class for storing scanned ticket IDs"""
@@ -14,7 +18,9 @@ class IdStorage:
         self.grace_period_s = grace_period_s
         self.storage: Dict[int, float] = {}
         self.file: Optional[TextIOWrapper] = None
+        self.writer: Optional[Any] = None
         self.persistence = persistence
+        self.event_name = "default"
 
     def __enter__(self) -> Any:
         self._save_open()
@@ -41,17 +47,40 @@ class IdStorage:
         self._save_open()
         self.persistence.persist_log_dir(log_dir)
 
+    def set_event_name(self, event_name: str) -> None:
+        self.event_name = event_name
+        if self.file is not None:
+            self.file.close()
+        self._save_open()
+
     def _add(self, ticket_id: int) -> None:
-        if self.file is None:
+        if self.file is None or self.writer is None:
             return
         now = time.time()
+        # inequality to now is fine here
+        time_stamp = datetime.now(timezone.utc).isoformat()
         self.storage[ticket_id] = now
-        local_time = time.localtime(now)
-        self.file.write(
-            f"{local_time.tm_year}-{local_time.tm_mon}-{local_time.tm_mday} {local_time.tm_hour}:{local_time.tm_min}:{local_time.tm_sec},{ticket_id}\n")
+        self.writer.writerow([time_stamp, ticket_id])
         self.file.flush()
 
     def _save_open(self) -> None:
         if not os.path.exists(self.log_dir):
             os.makedirs(self.log_dir)
-        self.file = open(self.log_dir / "ids.txt", "w", encoding="utf-8")
+        file_name = self.log_dir / f"{self.event_name}.txt"
+        # restore IDs from if log with the current event name exists and is less than 1 day old
+        if os.path.exists(file_name) and time.time() - os.stat(file_name).st_mtime < 60*60*24:
+            self.file = open(file_name, "r+", encoding="utf-8")
+            self._read_file()
+        else:
+            self.file = open(file_name, "w", encoding="utf-8")
+        self.writer = csv.writer(self.file, delimiter=',')
+
+    def _read_file(self) -> None:
+        if self.file is not None:
+            id_reader = csv.reader(self.file, delimiter=',')
+            for row in id_reader:
+                if len(row) != 0:
+                    date_time = iso8601.parse_date(row[0])
+                    epoch = datetime.fromtimestamp(0, timezone.utc)
+                    self.storage[int(row[1])] = (
+                        date_time - epoch).total_seconds()
